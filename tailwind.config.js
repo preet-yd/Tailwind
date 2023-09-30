8de3f5dd816044585426f50caf564b08030200d4
preet-yd <preet.yadav266@gmail.com>
2023-09-30 15:07:12 +0530

    added login.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,11 @@ module.exports = {
       height: {
         '39px': '39px',
         '355px': '355px',
-        '143x': '143px',
+        '143px': '143px',
+        '1px': '1px',
+        
+        'login-page-icon': '9.07738rem',
+        'login-page-100': '199px',
       },
       width: {
         '334': '20.875rem',
@@ -41,6 +45,10 @@ module.exports = {
         '512px': '512px',
         '482px': '482px',
         '598px': '598px',
+        'login-page-icon': '8.50806rem',
+        'login-page-100': '455px',
+        '172px': '172px',
+
       },
       gap: {
         '2.5': '0.625rem',
@@ -61,6 +69,7 @@ module.exports = {
         'card-component-signup-1': '12px 16px',
         'card-component-signup-2': '80px 20px 20px 20px',
         'tweet-component-frame': '8px 16px',
+        'login-page': '14.78125rem 10.121rem',
       },
     },
   },
